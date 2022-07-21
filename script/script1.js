ace5cdef923b5e4be4adfff4e297578cde5365ba
z3rohero <109614528+Z3rohero@users.noreply.github.com>
2022-07-21 19:56:21 +0000

    FUN

---
+++ script/script1.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){  
+  function redirrigir (){
+    setTimeout(function(){
+       window.location = 'src/entrada.html'; 
+      
+    }, 5000);
+   
+  }
+
+redirrigir();
+  
+})
+
